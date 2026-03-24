8c6870b984d474d3b09b9c39f41e4b092a1111e2
TobiasVeronaLaser <tobias.laser@ymail.com>
2026-03-24 15:37:08 +0100

    wip: improving load_ricu_concepts

---
+++ custom/r/testing.R
@@ -0,0 +1,10 @@
+library(jsonlite)
+
+found_data_info <- lapply(fromJSON("C:\\Users\\q039tl\\Documents\\OpenICU\\OpenICU-Example\\custom\\shared\\check-open_icu-to-ricu.json", simplifyVector=FALSE), function(x) toJSON(x, auto_unbox=TRUE))
+
+for (values in found_data_info) {
+  parsed <- fromJSON(values)
+  if(parsed[1,]$short == "abx"){
+    print("stop")
+  }
+}
