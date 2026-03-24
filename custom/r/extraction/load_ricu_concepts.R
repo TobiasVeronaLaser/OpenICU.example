bb3a4004185a660a38c20a3c77a4dfe1b69d9c28
TobiasVeronaLaser <tobias.laser@ymail.com>
2026-03-24 14:41:30 +0100

    feat: creating cnpt in R, compare to Ricu and save output

--- custom/r/extraction/load_ricu_concepts.R
+++ custom/r/extraction/load_ricu_concepts.R
@@ -4,7 +4,7 @@ library(jsonlite)
 output <- list()
 
 # JSON laden
-found_data_info <- lapply(fromJSON("C:\\Users\\tobi\\Documents\\check-open_icu-to-ricu.json", simplifyVector=FALSE), function(x) toJSON(x, auto_unbox=TRUE))
+found_data_info <- lapply(fromJSON("C:\\Users\\q039tl\\Documents\\OpenICU\\OpenICU-Example\\scripts\\check-open_icu-to-ricu.json", simplifyVector=FALSE), function(x) toJSON(x, auto_unbox=TRUE))
 
 local_patient <- as.data.frame(miiv$patients[, c("subject_id", "anchor_year", "anchor_age")])
 local_patient$birthdate <- as.POSIXct(
@@ -12,8 +12,8 @@ local_patient$birthdate <- as.POSIXct(
   format = "%Y-%m-%d %H:%M:%S", tz = "UTC"
 )
 
-blacklist = c("abx")
-wanted_list = c("ca")
+active_wanted_list = FALSE
+wanted_list = c("abx")
 
 for (values in found_data_info) {
   parsed <- fromJSON(values)
@@ -23,42 +23,62 @@ for (values in found_data_info) {
   cnpt_count = 0
   
   if (1 == length(parsed$short)){
-    if (parsed$short %in% blacklist) next
-    # if (!parsed$short %in% wanted_list ) next
+    if (active_wanted_list & !parsed$short %in% wanted_list ) next
     print(parsed$short)
-    
-    min_val <- if (length(parsed$min) > 0) as.numeric(parsed$min) else NULL
-    max_val <- if (length(parsed$max) > 0) as.numeric(parsed$max) else NULL
     
     cnpt <- load_concepts(parsed$short, src="miiv", aggregate=FALSE, interval=secs(1), id_type="patient")
     cnpt_count = nrow(cnpt)
     
+    min_val <- if (length(parsed$min) > 0) as.numeric(parsed$min) else NULL
+    max_val <- if (length(parsed$max) > 0) as.numeric(parsed$max) else NULL
+    
     cnpt$birthdate <- local_patient$birthdate[match(cnpt$subject_id, local_patient$subject_id)]
-    cnpt$charttime_origin <- cnpt$birthdate + cnpt$charttime - 3600
+    # cnpt$charttime_origin <- cnpt$birthdate + cnpt$charttime - 3600
     
-    tbl <- miiv[[parsed$table]][, c("itemid", "subject_id", "valuenum")]
+    tbl = miiv[[parsed$table]]
     
-    itemid_mask <- tbl[["itemid"]] %in% parsed$ids
+    if( -Inf != min_val | Inf != max_val){
+      tbl <- tbl[, c(parsed$sub_var, "subject_id", "valuenum")]
+    }else{
+      tbl <- tbl[, c(parsed$sub_var, "subject_id")]
+    }
+    
+    if(!is.null(parsed$ids) && length(parsed$ids) > 0){
+      itemid_mask <- tbl[[parsed$sub_var]] %in% parsed$ids
+    }else if(!is.null(parsed$src_regex) && length(parsed$src_regex) > 0){
+      itemid_mask <- grepl(parsed$src_regex, tbl[[parsed$sub_var]], ignore.case = TRUE)
+    } else {
+      cat("!NULL vor", parsed$short)
+      break
+    }
+    
+    # itemid_mask <- tbl[["itemid"]] %in% parsed$ids
     
     cnpt_from_event <- tbl[itemid_mask, ]
     cnpt_from_event_count = nrow(cnpt_from_event)
     
     # rm(tbl)
     
-    self_created_concept <- cnpt_from_event[!is.na(valuenum) & valuenum >= min_val & valuenum <= max_val]
+    if( -Inf != min_val | Inf != max_val){
+      # self_created_concept <- cnpt_from_event[!is.na(valuenum) & valuenum >= min_val & valuenum <= max_val]
+      self_created_concept <- cnpt_from_event[
+        !is.na(cnpt_from_event$valuenum) &
+          cnpt_from_event$valuenum >= min_val &
+          cnpt_from_event$valuenum <= max_val,
+      ]
+    }else{
+      self_created_concept <- cnpt_from_event
+    }
     self_created_concept_count = nrow(self_created_concept)
     
     # rm(cnpt, cnpt_from_event, self_created_concept, itemid_mask)
     # gc()
-    
-    
     
     if (cnpt_count == self_created_concept_count) {
       print(paste(parsed$short, ": works"))
     } else {
       print(paste(parsed$short, ": NOT", cnpt_count, "vs", self_created_concept_count))
     }
-    
     
     output[[parsed$name]] = list(
       name = parsed$name,
@@ -67,42 +87,65 @@ for (values in found_data_info) {
     )
     
   }else if (1 < length(parsed$short)){
-    if (parsed$short[1] %in% blacklist) next
-    # if (!parsed$short[1] %in% wanted_list ) next
+    if (active_wanted_list & !parsed$short[1] %in% wanted_list ) next
     print(parsed$short[1])
     
-    for (i in 1:length(parsed$short)){
+    cnpt <- load_concepts(parsed$short[1], src="miiv", aggregate=FALSE, interval=secs(1), id_type="patient")
+    cnpt_count = nrow(cnpt)
+    
+    for (i in seq_along(parsed$short)){
       
       min_val <- if (length(parsed$min[i]) > 0) as.numeric(parsed$min[i]) else NULL
       max_val <- if (length(parsed$max[i]) > 0) as.numeric(parsed$max[i]) else NULL
       
-      cnpt <- load_concepts(parsed$short[i], src="miiv", aggregate=FALSE, interval=secs(1), id_type="patient")
-      cnpt_count = cnpt_count + nrow(cnpt)
-      
       cnpt$birthdate <- local_patient$birthdate[match(cnpt$subject_id, local_patient$subject_id)]
-      cnpt$charttime_origin <- cnpt$birthdate + cnpt$charttime - 3600
+      # cnpt$charttime_origin <- cnpt$birthdate + cnpt$charttime - 3600
       
-      tbl <- miiv[[parsed$table[i]]][, c("itemid", "subject_id", "valuenum")]
+      tbl = miiv[[parsed$table[i]]]
+        
+      if( -Inf != min_val | Inf != max_val){
+        tbl <- tbl[, c(parsed$sub_var[i], "subject_id", "valuenum")]
+      }else{
+        tbl <- tbl[, c(parsed$sub_var[i], "subject_id")]
+      }
       
-      itemid_mask <- tbl[["itemid"]] %in% parsed$ids[i]
+      if(!is.null(parsed$ids[[i]]) && length(parsed$ids[[i]]) > 0){
+        itemid_mask <- tbl[[parsed$sub_var[i]]] %in% parsed$ids[[i]]
+      }else if(!is.null(parsed$src_regex[[i]]) && length(parsed$src_regex[[i]]) > 0){
+        itemid_mask <- grepl(parsed$src_regex[[i]], tbl[[parsed$sub_var[[i]]]], ignore.case = TRUE)
+      } else {
+        cat("!NULL vor", parsed$short[i])
+        break
+      }
       
       cnpt_from_event <- tbl[itemid_mask, ]
       cnpt_from_event_count = cnpt_from_event_count + nrow(cnpt_from_event)
       
       # rm(tbl)
       
-      self_created_concept <- cnpt_from_event[!is.na(valuenum) & valuenum >= min_val & valuenum <= max_val]
+      
+      if( -Inf != min_val | Inf != max_val){
+        # self_created_concept <- cnpt_from_event[!is.na(valuenum) & valuenum >= min_val & valuenum <= max_val]
+        self_created_concept <- cnpt_from_event[
+          !is.na(cnpt_from_event$valuenum) &
+            cnpt_from_event$valuenum >= min_val &
+            cnpt_from_event$valuenum <= max_val,
+        ]
+      }else{
+        self_created_concept <- cnpt_from_event
+      }
+      
       self_created_concept_count = self_created_concept_count + nrow(self_created_concept)
     }
-    # rm(cnpt, cnpt_from_event, self_created_concept, itemid_mask)
-    # gc()
-    
+
     if (cnpt_count == self_created_concept_count) {
       print(paste(parsed$short, ": works"))
     } else {
       print(paste(parsed$short, ": NOT", cnpt_count, "vs", self_created_concept_count))
     }
     
+    rm(cnpt, tbl, cnpt_from_event, self_created_concept, itemid_mask)
+    gc()
     
     output[[parsed$name[1]]] = list(
       name = parsed$name,
@@ -113,16 +156,8 @@ for (values in found_data_info) {
   }else{
     
   }
-  
-  if (1 <= length(parsed$short)){
-  }
-  
-  if (1 == length(parsed$short)){
-  }else if (1 < length(parsed$short)){
-  }
-  # break
   # gc()
   
 }
 
-write_json(output, "C:\\Users\\tobi\\Documents\\output.json", pretty = TRUE, auto_unbox = TRUE)
+write_json(output, "C:\\Users\\q039tl\\Documents\\output.json", pretty = TRUE, auto_unbox = TRUE)
